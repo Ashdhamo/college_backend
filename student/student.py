582c5de87eb05b3604213db79d6a23ebedb6f889
Ashdhamo <happyashwini2002@gmail.com>
2025-02-16 21:20:09 -0800

    allows searching of student

--- student/student.py
+++ student/student.py
@@ -22,3 +22,31 @@ def get_student():
 
     except mysql.connector.Error as err:
         return jsonify({"error": str(err)}), 500
+
+
+@student_blueprint.route('/search', methods=['POST'])
+def search_student():
+    try:           
+        data = request.get_json()
+        input_name = data.get('name', '').strip().lower()
+
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True) 
+
+        if not input_name:
+            cursor.execute("SELECT * FROM student;")
+            students = cursor.fetchall()
+        else:    
+
+            #initially I put input_name but it would literally search input_name 
+            query="SELECT * FROM student WHERE LOWER(name) LIKE %s;"
+            cursor.execute(query, (f"%{input_name}%",))
+            students = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return jsonify(students), 200
+
+    except mysql.connector.Error as err:
+        return jsonify({"error": str(err)}), 500
